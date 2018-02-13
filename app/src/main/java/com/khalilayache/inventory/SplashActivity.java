a6b15a09e0cb2754abd7a7172e09a5d92febbb89
khalilayache <khalilayache@gmail.com>
2018-02-13 14:43:44 -0200

    Add Splash Activity

--- app/src/main/java/com/khalilayache/inventory/SplashActivity.java
+++ app/src/main/java/com/khalilayache/inventory/SplashActivity.java
@@ -8,6 +8,6 @@ public class SplashActivity extends AppCompatActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_details);
+    setContentView(R.layout.activity_splash);
   }
 }
