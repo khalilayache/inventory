63e47a54175c1e2829332fbe002e514a297d9fe8
khalilayache <khalilayache@gmail.com>
2018-02-13 16:16:47 -0200

    Add Base Activity

---
+++ app/src/main/java/com/khalilayache/inventory/ui/base/BaseActivity.java
@@ -0,0 +1,9 @@
+package com.khalilayache.inventory.ui.base;
+
+import android.support.v7.app.AppCompatActivity;
+
+
+public class BaseActivity extends AppCompatActivity {
+
+  public static final Integer SPLASH_INTENT_TIME_DELAY = 500;
+}
